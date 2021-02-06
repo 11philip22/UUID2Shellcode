3c2b66a7c689f6c8248b5f6d6c3916e3c54b4efa
11philip22 <philipwoldhek@gmail.com>
2021-02-06 16:58:18 +0100

    changed some stuff

--- ShellcodeToUUID/BinToUUID.py
+++ ShellcodeToUUID/BinToUUID.py
@@ -2,23 +2,7 @@
 import sys
 import uuid
 
-
-def convert_to_uuid(shellcode):
-    # If shellcode is not in multiples of 16, then add some nullbytes at the end
-    if len(shellcode) % 16 != 0:
-        print("[-] Shellcode's length not multiplies of 16 bytes")
-        print("[-] Adding nullbytes at the end of shellcode, this might break your shellcode.")
-        print("\n[*] Modified shellcode length: ", len(shellcode) + (16 - (len(shellcode) % 16)))
-
-        add_nullbyte = b"\x00" * (16 - (len(shellcode) % 16))
-        shellcode += add_nullbyte
-
-    uuids = []
-    for i in range(0, len(shellcode), 16):
-        uuid_string = str(uuid.UUID(bytes_le=shellcode[i:i + 16]))
-        uuids.append('"' + uuid_string + '"')
-
-    return uuids
+from ShellcodeToUUID import convert_to_uuid
 
 
 def main(file_path):
